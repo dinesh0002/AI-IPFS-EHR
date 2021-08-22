ad3a0c3f94b5edaf1e1aa962d02050a378ce67ea
DINESH KUMAR MAMIDELA <dinesh.mamidella@gmail.com>
2021-08-22 12:26:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import styled from 'styled-components'
 import { AccountBox } from './components/accountBox';
 
 const AppContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
